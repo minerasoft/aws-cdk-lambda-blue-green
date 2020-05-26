c1c417076c550e8d3045b7f8fd6135e8a49f6365
Ram Anaswara <minerasoft@gmail.com>
2020-05-26 05:29:57 +0100

    feat: blue green deployment group for the lambda

--- lib/lambda-blue-green.ts
+++ lib/lambda-blue-green.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as codeDeploy from '@aws-cdk/aws-codedeploy'
+import {ILambdaDeploymentConfig} from "@aws-cdk/aws-codedeploy/lib/lambda/deployment-config";
 
 interface LambdaBlueGreenProps {
     /**
@@ -11,7 +13,13 @@ interface LambdaBlueGreenProps {
      * Name of the Alias used for the deployment of the lambda function.
      * Example: Prod
      */
-    lambdaAliasName: string
+    lambdaAliasName: string,
+    /**
+     * The Deployment Configuration for the Lambda.
+     *
+     * @default LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES
+     */
+    readonly deploymentConfig?: ILambdaDeploymentConfig;
 }
 
 export class LambdaBlueGreen extends cdk.Construct {
@@ -28,9 +36,14 @@ export class LambdaBlueGreen extends cdk.Construct {
             runtime: lambda.Runtime.NODEJS_12_X,
         });
 
-        new lambda.Alias(this, `${this.lambdaName}-Alias`, {
+        let alias = new lambda.Alias(this, `${this.lambdaName}-Alias`, {
             aliasName: props.lambdaAliasName,
             version: internalLambda.currentVersion,
+        });
+
+        new codeDeploy.LambdaDeploymentGroup(this, `${this.lambdaName}-DeploymentGroup`, {
+            alias,
+            deploymentConfig: props.deploymentConfig || codeDeploy.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
         });
     }
 }
--- test/lambda-blue-green.test.ts
+++ test/lambda-blue-green.test.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import '@aws-cdk/assert/jest';
 import {LambdaBlueGreen} from "../lib";
+import {LambdaDeploymentConfig} from "@aws-cdk/aws-codedeploy";
 
 function getSynthesisedStack(testApp: cdk.App) {
     return testApp.synth().getStackByName("TestStack");
@@ -8,26 +9,61 @@ function getSynthesisedStack(testApp: cdk.App) {
 
 describe('Lambda Blue Green Construct', () => {
     let testApp: cdk.App;
+    let testStack: cdk.Stack;
 
     beforeEach(() => {
         testApp = new cdk.App();
-        let testStack = new cdk.Stack(testApp, "TestStack");
-        new LambdaBlueGreen(testStack, 'CreateUser', {
-            handlerName: 'index.handler',
-            lambdaAliasName: 'live'
-        });
+        testStack = new cdk.Stack(testApp, "TestStack");
     })
 
-    it('should define a lambda function', () => {
-        expect(getSynthesisedStack(testApp)).toHaveResourceLike("AWS::Lambda::Function", {
-            Handler: "index.handler",
-            Runtime: "nodejs12.x"
-        });
-    });
-
-    it(`should have the 'live' alias`, () => {
-        expect(getSynthesisedStack(testApp)).toHaveResource("AWS::Lambda::Alias", {
-            Name: 'live'
+    describe('with default props values', () => {
+        beforeEach(() => {
+            new LambdaBlueGreen(testStack, 'CreateUser', {
+                handlerName: 'index.handler',
+                lambdaAliasName: 'live',
+            });
         })
-    });
+
+        it('should define a lambda function', () => {
+            expect(getSynthesisedStack(testApp)).toHaveResourceLike("AWS::Lambda::Function", {
+                Handler: "index.handler",
+                Runtime: "nodejs12.x"
+            });
+        });
+
+        it(`should have the 'live' alias`, () => {
+            expect(getSynthesisedStack(testApp)).toHaveResource("AWS::Lambda::Alias", {
+                Name: 'live'
+            })
+        });
+
+        it('should perform canary deployment by default',  () => {
+            expect(getSynthesisedStack(testApp)).toHaveResourceLike("AWS::CodeDeploy::DeploymentGroup", {
+                DeploymentConfigName: "CodeDeployDefault.LambdaCanary10Percent5Minutes",
+                DeploymentStyle: {
+                    DeploymentType: "BLUE_GREEN"
+                }
+            })
+        });
+
+    })
+
+    describe('with props values overridden', () => {
+        beforeEach(() => {
+            new LambdaBlueGreen(testStack, 'CreateUser-PropsOverridden', {
+                handlerName: 'index.handler',
+                lambdaAliasName: 'live',
+                deploymentConfig: LambdaDeploymentConfig.LINEAR_10PERCENT_EVERY_10MINUTES
+            });
+        });
+
+        it('should perform the blue/green deployment',  () => {
+            expect(getSynthesisedStack(testApp)).toHaveResourceLike("AWS::CodeDeploy::DeploymentGroup", {
+                DeploymentConfigName: "CodeDeployDefault.LambdaLinear10PercentEvery10Minutes",
+                DeploymentStyle: {
+                    DeploymentType: "BLUE_GREEN"
+                }
+            })
+        });
+    })
 });
